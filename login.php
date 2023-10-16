4d3fa7cda87dc078737373c4761727d6374b78bb
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2023-10-16 15:48:59 +0200

    :bug: [Login] Feat : form variables

--- login.php
+++ login.php
@@ -15,6 +15,8 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
 
 		// TODO
+		$email = $_POST['email'];
+		$password = $_POST['password'];
 
 		// Force user connection to access dashboard
 		userConnection($db, 'git@initiation.com', 'password');
